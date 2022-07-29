95eb3b18cdf88afd84004b6e95dd51ef101b2a74
Elna JP <eriosdd@gmail.com>
2022-07-29 00:48:14 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+function superbowlWin(record) {
+    let yearOfSuperBowlWin;
+    record.find(function(element,index,record)
+    {
+
+        if(record[index].result === "W")
+        {
+            yearOfSuperBowlWin = record[index].year
+            console.log(yearOfSuperBowlWin)
+            return yearOfSuperBowlWin
+        }
+    })
+    return yearOfSuperBowlWin
+  }
+console.log(superbowlWin(record))
+
+
+    
